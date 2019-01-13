a850404c35f220aef7c296bcf9dfc6dfccb4c585
Dries Swinnen <swinnendries@gmail.com>
2019-01-13 13:10:09 +0100

    initial commit

---
+++ 1002_helloworld2/02_helloworld2.js
@@ -0,0 +1,8 @@
+
+var message = 'Hello World';
+console.log(message);
+console.log(4 + 12);
+
+// werken met objecten
+var person = {firstname : 'Jan', lastname : 'Jansen'};
+console.log('Persoon: ', person.firstname + ' ' + person.lastname);
